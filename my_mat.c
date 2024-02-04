4a17b6d2f876e38391dd21ffae203dadeed0eb7a
Eylon <eylon@VM1.myguest.virtualbox.org>
2024-02-04 15:30:04 +0200

    started floyd warshall

--- my_mat.c
+++ my_mat.c
@@ -2,12 +2,27 @@
 
 void initMatrix(int mat[][MAT_SIZE])
 {
-    int i, j;
-    for (i = 0; i < 10; i++)
+    int i = 0, j = 0;
+    for (i = 0; i < MAT_SIZE; i++)
     {
-        for (j = 0; j < 10; j++)
+        for (j = 0; j < MAT_SIZE; j++)
         {
             scanf("%d", mat[i][j]);
         }
     }
+}
+
+int floydWarshallAlgorithm(int mat[][MAT_SIZE], int i, int j)
+{
+    int i = 0, j = 0, k = 0;
+    int matCopy[MAT_SIZE][MAT_SIZE] = {{0}};
+
+    for(int i = 0; i < MAT_SIZE; i++)
+    {
+        for(int j = 0; j < MAT_SIZE; j++)
+        {
+            matCopy[i][j] = mat[i][j];
+        }
+    }
+
 }
