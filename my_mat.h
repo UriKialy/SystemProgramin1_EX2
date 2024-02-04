e8417defb6af86737ee9dec6c809d5724d08d7b1
Eylon <eylon@VM1.myguest.virtualbox.org>
2024-02-04 15:58:16 +0200

    changed h file

--- my_mat.h
+++ my_mat.h
@@ -1,3 +1,9 @@
 #include <stdio.h>
 
 #define MAT_SIZE 10
+
+/*the function receives a matrix, returns an initiallized matrix*/
+void initMatrix(int mat[][MAT_SIZE]);
+
+/*the function will calculate the shortest path from i to j using the Floyd-Warshall algorithm*/
+int floydWarshallAlgorithm(int mat[][MAT_SIZE], int i, int j);
