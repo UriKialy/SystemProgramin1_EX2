e081c7aba1ea39831693381527cd040f205c35c0
Eylon <eylon@VM1.myguest.virtualbox.org>
2024-02-06 19:12:47 +0200

    added run-loop

--- my_graph.c
+++ my_graph.c
@@ -6,32 +6,33 @@ int main(int argc, char *argv[])
     char selection = 0, i = 0, j = 0;
     int mat[MAT_SIZE][MAT_SIZE] = {{0}};
 
-    selection = getchar();
-
-    switch (selection)
+    while ((selection = getchar()) != 'D' || selection != EOF)
     {
-    case 'A':
-        initMatrix(mat);
-        break;
-    case 'B':
-        i = getchar();
-        j = getchar();
+        switch (selection)
+        {
+        case 'A':
+            initMatrix(mat);
+            break;
+        case 'B':
+            i = getchar();
+            j = getchar();
 
-        floydWarshallAlgorithm(mat, i, j) ? printf("True\n") : printf("False\n");
-        break;
-    case 'C':
-        i = getchar();
-        j = getchar();
+            floydWarshallAlgorithm(mat, i, j) ? printf("True\n") : printf("False\n");
+            break;
+        case 'C':
+            i = getchar();
+            j = getchar();
 
-        len = floydWarshallAlgorithm(mat, i, j) ? printf("%d\n", len) : printf("-1\n");
-        break;
-    case 'D':
-    case EOF:
-        break;
+            len = floydWarshallAlgorithm(mat, i, j) ? printf("%d\n", len) : printf("-1\n");
+            break;
+        case 'D':
+        case EOF:
+            break;
 
-    default:
-        break;
+        default:
+            break;
+        }
     }
-    
+
     return 0;
 }
