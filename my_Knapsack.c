586cffe9509d93eb5d7b758c2fa6752285528ade
Eylon <eylon@VM1.myguest.virtualbox.org>
2024-02-07 14:15:31 +0200

    finished knapsack

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -3,7 +3,71 @@
 #define ITEMS 5
 #define WEIGHT 20
 
+int max(int a, int b){ return (a > b) ? a : b;}
 
 int knapSack(int weights[], int values[] , int selected_bool[])
 {
+    int i = 0, sum = 0, currItem = 0,currWeight = 0, itemVal = 0, itemWeight = 0, inSum = 0;
+    int mat[WEIGHT][ITEMS] ={{0}};
 
+    for (currItem = 1; currItem < ITEMS; currItem++)
+    {
+        itemVal = values[currItem];
+        itemWeight = weights[currItem];
+
+        for (currWeight = 0; currWeight < WEIGHT; currWeight++)
+        {
+            if (itemWeight > currWeight)
+            {
+                mat[currItem][currWeight] = mat[currItem - 1][currWeight];
+            }
+            else
+            {
+                mat[currItem][currWeight] = max(mat[currItem - 1][currWeight], mat[currItem - 1][currWeight - itemWeight] + itemVal);
+            }
+        }
+    }
+
+    currWeight = WEIGHT;
+
+    for (i = ITEMS - 1; i <= 0; i--)
+    {
+        inSum = (mat[i][WEIGHT - 1] != mat[i - 1][WEIGHT - 1]);
+
+        if(inSum || currWeight >= 0)
+        {
+            selected_bool[i] = 1;
+            sum += values[i];
+            currWeight -= weights[i];
+        }
+
+    }
+
+    return sum;
+}
+
+int main(int argc, char* argv[])
+{
+    int indexer = 0;
+    int result[ITEMS] = {0};
+    int itemWeights[ITEMS] = {0};
+    int itemValues[ITEMS] = {0};
+
+    do
+    {
+        indexer = (int)(getchar() - 'a');
+        scanf("%d", &itemValues[indexer]);
+        scanf("%d", &itemWeights[indexer]);
+    } while (indexer <= ITEMS - 1);
+    
+    
+    printf("Maximum profit: %d", knapSack(itemWeights,itemValues,result));
+    printf("Selected Items: ");
+    for (indexer = 0; indexer < ITEMS - 1; indexer++)
+    {
+        if(result[indexer]) {printf("%c ", (char)(indexer + 'a'));}
+    }
+    if(result[ITEMS - 1]) {printf("%c", (char)((ITEMS - 1) + 'a'));}
+
+    return 0;
+}
