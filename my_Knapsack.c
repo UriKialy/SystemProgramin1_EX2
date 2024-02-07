88af0412bb13230f1b90cd0dd1e0ef6b9513054a
Eylon <eylon@VM1.myguest.virtualbox.org>
2024-02-08 00:25:14 +0200

    fixed

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -34,7 +34,7 @@ int knapSack(int weights[], int values[] , int selected_bool[])
     {
         inSum = (mat[i][currWeight] != mat[i - 1][currWeight]);
 
-        if(inSum)
+        if(inSum && currWeight >= 0)
         {
             selected_bool[i] = 1;
             sum += values[i];
